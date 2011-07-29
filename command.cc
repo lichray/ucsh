2de13522d7971b121a6565fd5edf6d1b48b012ef
Zhihao Yuan <lichray@gmail.com>
2011-07-29 05:39:49 -0500

    Supports redirection; allows error in multi-cmd.

--- command.cc
+++ command.cc
@@ -10,6 +10,7 @@
 #include <glob.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 // print all unexpended commands, one command per line
 int ucsh::CommandGroup::print() {
@@ -19,9 +20,10 @@ int ucsh::CommandGroup::print() {
 }
 
 int ucsh::CommandGroup::execute() {
-	int st = 0, wst = 0;
+	int st, wst;
 	int ofd = 0;
 	for (iterator i = this->begin(); i != this->end(); ++i) {
+		st = 0;
 		glob_t cbuf;
 		for (size_t j = 0; j < i->args.size(); ++j)
 			if (GLOB_NOMATCH == glob(i->args[j].c_str(),
@@ -56,8 +58,38 @@ int ucsh::CommandGroup::execute() {
 					close(fd[1]);
 					dup2(ofd, 0);
 					close(ofd);
+				} else if (RDR_R <= i->opt and i->opt <= RDR_A) {
+					glob_t rbuf;
+					if (GLOB_NOMATCH == glob((++i)->args[0].c_str(),
+							GLOB_NOMAGIC | GLOB_TILDE, NULL, &rbuf)) {
+						globfree(&rbuf);
+						P_ERRF(i->args[0].c_str(), "No match");
+						exit(1);
+					}
+					if (i->args.size() > 1 or rbuf.gl_pathc > 1) {
+						P_ERRF(*cbuf.gl_pathv, "Too many targets");
+						exit(1);
+					}
+					int rf;
+					switch ((i-1)->opt) {
+					case RDR_R:
+						rf = open(*rbuf.gl_pathv, O_RDONLY);
+						dup2(rf, 0);
+						close(rf);
+						break;
+					case RDR_W:
+						rf = creat(*rbuf.gl_pathv, 0666);
+						goto rdr_to;
+					case RDR_A:
+						rf = open(*rbuf.gl_pathv, O_WRONLY | O_APPEND);
+					rdr_to:
+						dup2(rf, 1);
+						close(rf);
+					default:;
+					}
+					globfree(&rbuf);
 				}
-				if(execvp(*cbuf.gl_pathv, cbuf.gl_pathv)) {
+				if (execvp(*cbuf.gl_pathv, cbuf.gl_pathv)) {
 					if (errno == ENOENT) // "no fuch file" in $PATH
 						P_ERRF(*cbuf.gl_pathv, "Command not found");
 					else perror(*cbuf.gl_pathv);
@@ -73,6 +105,8 @@ int ucsh::CommandGroup::execute() {
 				ofd = fd[0];  // preserved old output
 				close(fd[1]); // send EOF to input
 				break;
+			case RDR_R: case RDR_W: case RDR_A:
+				++i;
 			default:
 				waitpid(pid, &wst, WCONTINUED);
 				st = WEXITSTATUS(wst);
